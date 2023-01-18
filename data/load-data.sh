1e2e8b68dc0abcd8bcbf4baef75f7c07fffd965d
Jake Holmquist <jake.holmquist@gmail.com>
2023-01-18 16:52:53 -0500

    adding data directory

---
+++ data/load-data.sh
@@ -0,0 +1,5 @@
+gsutil cp *.csv gs://bq-jake-cysl
+bq load --source_format=CSV --skip_leading_rows=1 CYSL.Program gs://bq-jake-cysl/CYSL\ Schema\ -\ Program.csv
+bq load --source_format=CSV --skip_leading_rows=1 CYSL.Season gs://bq-jake-cysl/CYSL\ Schema\ -\ Season.csv
+bq load --source_format=CSV --skip_leading_rows=1 CYSL.Team gs://bq-jake-cysl/CYSL\ Schema\ -\ Team.csv
+
